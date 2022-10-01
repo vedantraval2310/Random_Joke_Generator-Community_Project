fe51ac8eabeef6f2deaea646783634649b0ba45a
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2022-10-02 02:37:28 +0530

    API Working || Error remove

--- script.js
+++ script.js
@@ -5,25 +5,32 @@ const copy = document.querySelector("#copy_joke");
 
 
 const jokeContainer = document.getElementById("joke");
-const btn = document.getElementById("btn");
-const url = "https://icanhazdadjoke.com/";
 
-let getJoke = () => {
+
+const btn = document.getElementById("btn");
+const url = "https://icanhazdadjoke.com/slack";
+
+let getJoke = () =>  {
     jokeContainer.classList.remove("fade");
-    fetch(url)
-    .then(data => data.json())
-    .then(item =>{
-        jokeContainer.textContent = `${item.joke}`;
-        jokeContainer.classList.add("fade");
+
+
+     fetch(url) 
+    .then((response) => response.json()) 
+    .then((user) => {
+      let j = user.attachments[0].text;
+      // console.log(user.attachments[0].text); 
+      jokeContainer.textContent = j;
+      jokeContainer.classList.add("fade");
     });
 }
+
 btn.addEventListener("click",getJoke);
 getJoke();
   
   // Fade in 
   setTimeout(function(){ 
-     jokeEl.innerHTML = joke.joke; 
-      jokeEl.style.opacity = 1;
+     jokeContainer.innerHTML = "Get another joke" 
+      jokeContainer.style.opacity = 1;
   },500);
      
 
@@ -33,7 +40,7 @@ toggle.addEventListener("click", () => {
     ? (toggle.firstElementChild.className = "far fa-moon")
     : (toggle.firstElementChild.className = "far fa-sun");
 });
- //master
+ 
 
  copy.addEventListener("click", () => {
   const text = jokeContainer.textContent;
